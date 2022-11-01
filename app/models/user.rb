f2adbf63f882d7d552ca86d6af97d3629f8659a6
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-11-01 17:38:29 +0100

    Remove try out methods from User

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,3 @@ end
 def recent_posts
    Post.where(author_id: id).order(created_at: :desc).limit(3)
 end
-
-User.create(name: 'Tom', last_name: 'Smith', age: '37') # Create
-User.find_by(name: 'Tom') # Read
-User.find(1).update(name: 'Ann') # Update
-User.find_by(name: 'Ann').delete # Delete
