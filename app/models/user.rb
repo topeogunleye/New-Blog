a8c3937b023af00d021109a55bd57e60ba74cf2d
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-11-01 15:15:27 +0100

    Add a method that returns the 3 most recent posts for a given user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'author_id'
 end
 
+def recent_posts
+   Post.where(author_id: id).order(created_at: :desc).limit(3)
+end
+
 User.create(name: 'Tom', last_name: 'Smith', age: '37') # Create
 User.find_by(name: 'Tom') # Read
 User.find(1).update(name: 'Ann') # Update
