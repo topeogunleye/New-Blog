a84dcd50fe187f89ace1b91ca9c69fb1db8924cf
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-11-01 17:59:02 +0100

    Fix linter errors

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,9 +2,7 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
-
-  def update_likes_counter 
+  def update_likes_counter
     Post.increment_counter(:likes_counter, post_id)
   end
 end
-
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,8 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
-  # returns the 3 most recent posts for a given user. 
+  # returns the 3 most recent posts for a given user.
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
 end
-
