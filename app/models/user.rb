6b505f08cfa187b96d97d793c3beb8c1406403bc
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-11-01 13:29:29 +0100

    Add methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,3 +3,8 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 end
+
+User.create(first_name: 'Tom', last_name: 'Smith', age: '37') # Create
+User.find_by(first_name: 'Tom') # Read
+User.find(1).update(first_name: 'Ann') # Update
+User.find_by(first_name: 'Tom').delete # Delete
