be6dc9027db97a7d253e005d2f8a049937307578
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-11-01 15:24:22 +0100

    Add a method that returns the recent comments for a post and a method that updates the posts counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,14 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+
+  def recent_comments
+    Comments.where(post_id: id).order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def update_posts_counter
+    User.increment_counter(:posts_counter, author_id)
+  end
 end
