d3702557fac4ba212710a0ae2e5efc07e8565bfe
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-11-04 11:30:27 +0100

    Add block to comment_spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -6,7 +6,8 @@ RSpec.describe Post, type: :model do
     @post = Post.create(author_id: @user.id, title: 'My first post', text: 'A good post')
   end
 
-
-  
+  it 'should increment the comments counter' do
+    expect { Comment.create(author_id: @user.id, post_id: @post.id, text: 'A good comment') }.to change { @post.reload.comments_counter }.by(1)
+  end
 
 end
