2d6be997216bb83bbef8882b0cb46472052440aa
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-11-05 17:52:19 +0100

    Add current user method to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+#   Create a method called current_user in your ApplicationController to make current user data available to all controllers.
+# It will return the first user from the database.
+
+def current_user
+  @current_user ||= User.first
+end
+
 end
