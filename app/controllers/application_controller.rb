ce4c3c617bbe495f144f7e66965598391c8d5f15
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-11-05 18:57:38 +0100

    Fix linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,5 @@
 class ApplicationController < ActionController::Base
-
-#   Create a method called current_user in your ApplicationController to make current user data available to all controllers.
-# It will return the first user from the database.
-
-def current_user
-  @current_user ||= User.first
-end
-
+  def current_user
+    @current_user ||= User.first
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -43,7 +43,8 @@ class PostsController < ApplicationController
   end
 
   private
-    def post_params
-      params.require(:post).permit(:title, :text)
-    end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
